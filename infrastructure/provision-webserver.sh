be165b87ba1c560cddcdbc16644b87181efe7062
puscasu <iulian.p234@hotmail.com>
2020-04-09 17:18:42 +0100

    server now runs as user instead of root

--- infrastructure/provision-webserver.sh
+++ infrastructure/provision-webserver.sh
@@ -6,12 +6,12 @@ source $HOME/instrumentation/java-config-rnl-vm.sh
 shopt -s globstar # '**' will expand to more than one directory
 javac $HOME/instrumentation/**/*.java
 javac $HOME/webserver/**/solver/*.java
-javac $HOME/webserver/**/server/WebServer.java
 mkdir -p $HOME/webserver-instrumented/pt/ulisboa/tecnico/cnv/solver
 java StatisticsTool -load_store  $HOME/webserver/pt/ulisboa/tecnico/cnv/solver $HOME/webserver-instrumented/pt/ulisboa/tecnico/cnv/solver
+javac $HOME/webserver/**/server/WebServer.java
 
 # Java server
 mkdir $HOME/logs
 touch $HOME/logs/server.log
-echo "java -cp $HOME/webserver-instrumented:$HOME/webserver:$HOME/instrumentation:$HOME/instrumentation/bit-samples pt.ulisboa.tecnico.cnv.server.WebServer >> $HOME/logs/server.log 2>&1" | sudo tee -a /etc/rc.local
+echo "su - ec2-user -c 'java -cp $HOME/webserver-instrumented:$HOME/webserver:$HOME/instrumentation:$HOME/instrumentation/bit-samples pt.ulisboa.tecnico.cnv.server.WebServer >> $HOME/logs/server.log 2>&1'" | sudo tee -a /etc/rc.local
 sudo chmod +x /etc/rc.local
